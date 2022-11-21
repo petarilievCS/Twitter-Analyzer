5dde7cfdae81759c72c2b6fd63437faf0f8a67a3
Petar Iliev <petariliev2002@gmail.com>
2022-11-21 11:39:46 -0600

    Implement data fetching from TwitterAPI

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import SwifteriOS
+import CoreML
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
+    // setup
+    let swifter = Swifter(consumerKey: K.consumerKey, consumerSecret: K.secretKey)
+    let classifier = TwitterAnalyzer()
+
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
@@ -17,11 +24,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // testing API
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended) { results, metadata in
+            let decoder = JSONDecoder()
+            do {
+                // get tweets
+                var tweets: [String] = []
+                for i in 0..<100 {
+                    if let tweet = results[i]["full_text"].string {
+                        tweets.append(tweet)
+                    }
+                }
+            } catch {
+                print("Error while decoding data: \(error.localizedDescription)")
+            }
+        } failure: { error in
+            print("Error in Twitter API request: \(error.localizedDescription)")
+        }
+        
     }
 
     @IBAction func predictPressed(_ sender: Any) {
-    
-    
+        
     }
     
 }
