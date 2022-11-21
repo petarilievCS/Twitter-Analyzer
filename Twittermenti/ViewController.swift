e8a223357320e8eacbcc146caca288d522e0913a
Petar Iliev <petariliev2002@gmail.com>
2022-11-21 12:05:53 -0600

    Refactor code

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -16,11 +16,11 @@ class ViewController: UIViewController {
     // setup
     let swifter = Swifter(consumerKey: K.consumerKey, consumerSecret: K.secretKey)
     let classifier = TwitterAnalyzer()
-
+    
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,58 +28,69 @@ class ViewController: UIViewController {
         let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
     }
-
+    
     @IBAction func predictPressed(_ sender: Any) {
+        fetchTweets()
+    }
+    
+    // Fetches data from TwitterAPI
+    func fetchTweets() {
         if let searchTerm = textField.text {
-            swifter.searchTweet(using: searchTerm, lang: "en", count: 100, tweetMode: .extended) { results, metadata in
+            swifter.searchTweet(using: searchTerm, lang: "en", count: K.tweetCount, tweetMode: .extended) { results, metadata in
                 let decoder = JSONDecoder()
-                do {
-                    // get tweets
-                    var tweets: [TwitterAnalyzerInput] = []
-                    for i in 0..<100 {
-                        if let tweet = results[i]["full_text"].string {
-                            tweets.append(TwitterAnalyzerInput(text: tweet))
-                        }
+                // get tweets
+                var tweets: [TwitterAnalyzerInput] = []
+                for i in 0..<K.tweetCount {
+                    if let tweet = results[i]["full_text"].string {
+                        tweets.append(TwitterAnalyzerInput(text: tweet))
                     }
-                    // make batch prediction
-                    do {
-                        let predictions = try self.classifier.predictions(inputs: tweets)
-                        var score = 0
-                        for prediction in predictions {
-                            switch prediction.label {
-                            case "Pos":
-                                score += 1
-                            case "Neg":
-                                score -= 1
-                            default:
-                                break
-                            }
-                        }
-                        // update UI
-                        if score > 20 {
-                            self.sentimentLabel.text = "🥰"
-                        } else if score > 10 {
-                            self.sentimentLabel.text = "😀"
-                        } else if score > 0 {
-                            self.sentimentLabel.text = "🙂"
-                        } else if score == 0 {
-                            self.sentimentLabel.text = "😐"
-                        } else if score > -10 {
-                            self.sentimentLabel.text = "🙁"
-                        } else if score > -20 {
-                            self.sentimentLabel.text = "😠"
-                        } else {
-                            self.sentimentLabel.text = "😡"
-                        }
-                    } catch {
-                        print("Prediction error: \(error.localizedDescription)")
-                    }
-                } catch {
-                    print("Error while decoding data: \(error.localizedDescription)")
                 }
+                self.makePrediction(with: tweets)
             } failure: { error in
                 print("Error in Twitter API request: \(error.localizedDescription)")
             }
+        }
+    }
+    
+    // Makes prediction using CoreML model
+    func makePrediction(with tweets: [TwitterAnalyzerInput]) {
+        // make batch prediction
+        do {
+            let predictions = try self.classifier.predictions(inputs: tweets)
+            var score = 0
+            for prediction in predictions {
+                switch prediction.label {
+                case "Pos":
+                    score += 1
+                case "Neg":
+                    score -= 1
+                default:
+                    break
+                }
+            }
+            updateUI(with: score)
+        } catch {
+            print("Prediction error: \(error.localizedDescription)")
+        }
+    }
+    
+    // updates UI 
+    func updateUI(with score: Int) {
+        // update UI
+        if score > 20 {
+            self.sentimentLabel.text = "🥰"
+        } else if score > 10 {
+            self.sentimentLabel.text = "😀"
+        } else if score > 0 {
+            self.sentimentLabel.text = "🙂"
+        } else if score == 0 {
+            self.sentimentLabel.text = "😐"
+        } else if score > -10 {
+            self.sentimentLabel.text = "🙁"
+        } else if score > -20 {
+            self.sentimentLabel.text = "😠"
+        } else {
+            self.sentimentLabel.text = "😡"
         }
     }
     
